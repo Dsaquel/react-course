8702e4bd27e4db7196022b3524ab4543e29704fb
Dsaquel <noblet.ouwaysgta5@gmail.com>
2023-03-10 11:47:36 +0100

    :sparkles: add condition

--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -4,12 +4,27 @@ import Result from '../components/converter/Result'
 
 const Converter = () => {
   const [result, setResult] = useState('')
+  const [istitleShow, setIsTitleShow] = useState(true)
 
   return (
     <div>
       <h1>Converter</h1>
       <FormConverter setResult={setResult} />
       <Result result={result} />
+
+      <div
+        style={{
+          margin: '20px',
+        }}
+      >
+        {istitleShow ? <h2>Titre</h2> : null}
+
+        {istitleShow && <h2>Titre sans else</h2>}
+
+        <button onClick={() => setIsTitleShow(!istitleShow)}>
+          Cacher le titre
+        </button>
+      </div>
     </div>
   )
 }
